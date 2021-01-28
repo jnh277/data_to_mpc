bde35d619a8fefbde44ad24c7b551d8797a83e66
jnh277 <johannes.n.hendriks@gmail.com>
2021-01-29 10:44:26 +1100

    Update jax_test.py
    Examples of computing Jacobian and hessian using Jax

--- jax_test.py
+++ jax_test.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 import pickle
-from scipy.optimize import minimize
+from scipy.optimize import minimize, least_squares
 
 import jax.numpy as jnp
-from jax import grad, jit, device_put
+from jax import grad, jit, device_put, jacfwd, jacrev, jvp
 
 
 a = jnp.array([-2,1.5])
@@ -14,9 +14,9 @@ def cost(x, a, c):
     return jnp.power(x-a,2).sum() + c
 
 
-cost_jit = jit(cost, static_argnums=(2,))
+cost_jit = jit(cost)
 gradient = grad(cost_jit, argnums=0)
-
+hessian = jacfwd(jacrev(cost_jit, argnums=0))
 
 def npgradient(x, *args): # need this wrapper for scipy.optimize.minimize
     return 0+np.asarray(gradient(x, *args))  # adding 0 since 'L-BFGS-B' otherwise complains about contig. problems ...
@@ -24,10 +24,30 @@ def npgradient(x, *args): # need this wrapper for scipy.optimize.minimize
 
 x0 = jnp.ones((2,))
 
-
-res = minimize(cost_jit, x0, jac=npgradient, args=(a, c))
-
+# providing gradient and hessian to optimisation routine
+res = minimize(cost_jit, x0, jac=npgradient, args=(a, c), method='Newton-CG', hess=hessian, options={'disp':True})
 print(res.x)
 
+
+# look at computing the jacobian and how this might be used in a least squares setting
+a = device_put(np.random.uniform(0.5, 1.5, 5))
+A = jnp.stack([a, a**2]).T
+x = jnp.array([1.0, 2.0])
+b = A @ x
+
+def error_func(x, A, b):
+    return b - jnp.matmul(A, x)
+
+error_func_jit = jit(error_func)
+
+# use forward mode differentiation when jacobian will be tall
+jac1 = jacfwd(error_func_jit, argnums=0)
+
+# use reverse mode differentiation when jacobian will be fat
+jac2 = jacrev(error_func_jit, argnums=0)
+x0 = np.array([0,0])
+
+res2 = least_squares(error_func_jit, x0, jac=jac1, args=(A,b))
+print(res2)
 
 
