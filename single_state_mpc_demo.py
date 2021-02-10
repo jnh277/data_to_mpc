e26b085afe720a54250a3a34c4e1d82149aeddf4
johannes <johannes.n.hendriks@gmail.com>
2021-02-11 10:30:15 +1100

    Working 1d demo

--- single_state_mpc_demo.py
+++ single_state_mpc_demo.py
@@ -44,17 +44,18 @@ config.update("jax_enable_x64", True)           # run jax in 64 bit mode for acc
 # Control parameters
 x_star = np.array([1.0])        # desired set point
 M = 200                             # number of samples we will use for MC MPC
-N = 20                              # horizonline of MPC algorithm
+N = 10                              # horizonline of MPC algorithm
 sqc = np.array([[1.0]])             # square root cost on state error
 src = np.array([[0.1]])             # square root cost on control action
 delta = 0.05                        # desired maximum probability of not satisfying the constraint
 x_ub = 1.2
+u_ub = 3.0
 state_constraints = (lambda x: x_ub - x,)
-input_constraints = (lambda u: 5.0 - u,)
+input_constraints = (lambda u: u_ub - u,)
 
 
 # simulation parameters
-T = 10             # number of time steps to simulate and record measurements for
+T = 30              # number of time steps to simulate and record measurements for
 x0 = 0.5            # initial time step
 r_true = 0.01       # measurement noise standard deviation
 q_true = 0.05       # process noise standard deviation
@@ -69,7 +70,7 @@ w = np.random.normal(0.0, q_true, T+1)        # make a point of predrawing noise
 y = np.zeros((T,))
 
 # create some inputs that are random but held for 10 time steps
-u = np.zeros((T,))     # first control action will be zero
+u = np.zeros((T+1,))     # first control action will be zero
 
 ### hmc parameters and set up the hmc model
 warmup = 1000
@@ -103,7 +104,11 @@ gradient = jit(grad(log_barrier_cost, argnums=0), static_argnums=(11, 12, 13, 14
 hessian = jit(jacfwd(jacrev(log_barrier_cost, argnums=0)), static_argnums=(11, 12, 13, 14, 15))
 
 
-
+xt_est_save = np.zeros((1,M,T))
+a_est_save = np.zeros((M,T))
+b_est_save = np.zeros((M,T))
+q_est_save = np.zeros((M,T))
+r_est_save = np.zeros((M,T))
 ### SIMULATE SYSTEM AND PERFORM MPC CONTROL
 for t in range(T):
     # simulate system
@@ -120,7 +125,7 @@ for t in range(T):
         'u': u[:t+1],
         'prior_mu': np.array([0.8, 0.05, 0.1, 0.1]),
         'prior_std': np.array([0.2, 0.2, 0.2, 0.2]),
-        'prior_state_mu': 0.5,
+        'prior_state_mu': 0.3,
         'prior_state_std': 0.2,
     }
 
@@ -143,23 +148,73 @@ for t in range(T):
     ut = np.expand_dims(np.array([u[t]]), 0)  # control action that was just applied
     theta = {'a': a,
              'b': b, }
+
+    # save some things for later plotting
+    xt_est_save[0,:,t] = z[:, -1]
+    a_est_save[:, t] = a
+    b_est_save[:, t] = b
+    q_est_save[:, t] = q
+    r_est_save[:, t] = r
+
     # calculate next control action
     result = solve_chance_logbarrier(np.zeros((1, N)), cost, gradient, hessian, ut, xt, theta, w, x_star, sqc, src,
-                                     delta, simulate, state_constraints, input_constraints, verbose=2)
+                                     delta, simulate, state_constraints, input_constraints, verbose=True)
 
     uc = result['uc']
     u[t+1] = uc[0,0]
 
 
 plt.subplot(2,1,1)
-plt.plot(x)
+plt.plot(x,label='True', color='k')
+plt.plot(xt_est_save[0,:,:].mean(axis=0), color='b',label='mean')
+plt.plot(np.percentile(xt_est_save[0,:,:],97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
+plt.plot(np.percentile(xt_est_save[0,:,:],2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
 plt.ylabel('x')
+plt.axhline(x_ub,linestyle='--',color='r',linewidth=2.,label='constraint')
+plt.axhline(x_star,linestyle='--',color='g',linewidth=2.,label='target')
+plt.legend()
 
 plt.subplot(2,1,2)
 plt.plot(u)
+plt.axhline(u_ub,linestyle='--',color='r',linewidth=2.,label='constraint')
 plt.ylabel('u')
+plt.xlabel('t')
 
 plt.tight_layout()
 plt.show()
 
+plt.subplot(2,2,1)
+plt.plot(a_est_save.mean(axis=0),label='mean')
+plt.plot(np.percentile(a_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
+plt.plot(np.percentile(a_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
+plt.ylabel('a')
+plt.axhline(0.9,linestyle='--',color='k',linewidth=2.,label='true')
+plt.legend()
 
+plt.subplot(2,2,2)
+plt.plot(b_est_save.mean(axis=0),label='mean')
+plt.plot(np.percentile(b_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
+plt.plot(np.percentile(b_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
+plt.ylabel('b')
+plt.axhline(0.1,linestyle='--',color='k',linewidth=2.,label='true')
+plt.legend()
+
+plt.subplot(2,2,3)
+plt.plot(q_est_save.mean(axis=0),label='mean')
+plt.plot(np.percentile(q_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
+plt.plot(np.percentile(q_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
+plt.ylabel('q')
+plt.axhline(q_true,linestyle='--',color='k',linewidth=2.,label='true')
+plt.legend()
+plt.xlabel('t')
+
+plt.subplot(2,2,4)
+plt.plot(r_est_save.mean(axis=0),label='mean')
+plt.plot(np.percentile(r_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
+plt.plot(np.percentile(r_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
+plt.ylabel('r')
+plt.axhline(r_true,linestyle='--',color='k',linewidth=2.,label='true')
+plt.legend()
+plt.xlabel('t')
+
+plt.show()
