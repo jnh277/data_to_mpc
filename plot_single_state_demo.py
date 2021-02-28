50c756b4379de0fc3b3acf37767fd900273ead99
jnh277 <johannes.n.hendriks@gmail.com>
2021-03-01 10:58:37 +1100

    Update plot_single_state_demo.py

--- plot_single_state_demo.py
+++ plot_single_state_demo.py
@@ -35,6 +35,7 @@ import jax.numpy as jnp
 from jax import grad, jit, device_put, jacfwd, jacrev
 from jax.ops import index, index_add, index_update
 from jax.config import config
+import seaborn as sns
 
 # optimisation module imports (needs to be done before the jax confix update)
 from optimisation import log_barrier_cost, solve_chance_logbarrier
@@ -122,67 +123,126 @@ with open('results/'+run+'/mpc_result_save100.pkl', 'rb') as file:
     mpc_result_save = pickle.load(file)
 
 
-
+fontsize=14
 plt.subplot(2,1,1)
 #print(plt.rcParams['axes.prop_cycle'].by_key()['color'])
 plt.fill_between(np.arange(T),np.percentile(xt_est_save[0,:,:],97.5,axis=0),np.percentile(xt_est_save[0,:,:],2.5,axis=0),alpha=0.2,label='95% CI',color=u'#1f77b4')
-plt.plot(x,label='True', color='k')
-plt.plot(xt_est_save[0,:,:].mean(axis=0),label='mean',color=u'#1f77b4',linestyle='--')
+plt.plot(x,label='True', color='k',linewidth=2.)
+plt.plot(xt_est_save[0,:,:].mean(axis=0),linewidth=2.,label='mean',color=u'#1f77b4',linestyle='--')
 # plt.plot(np.percentile(xt_est_save[0,:,:],97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
 # plt.plot(np.percentile(xt_est_save[0,:,:],2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
-plt.ylabel('x')
+plt.ylabel('x', fontsize=fontsize)
+plt.xticks([])
 plt.axhline(x_ub,linestyle='--',color='r',linewidth=2.,label='constraint')
 plt.axhline(x_star,linestyle='--',color='g',linewidth=2.,label='target')
-plt.legend()
+plt.legend(fontsize=12)
 
 plt.subplot(2,1,2)
-plt.plot(u)
+plt.plot(u,linewidth=2., color='k')
 plt.axhline(u_ub,linestyle='--',color='r',linewidth=2.,label='constraint')
-plt.ylabel('u')
-plt.xlabel(r'$t$')
+plt.ylabel('u', fontsize=fontsize)
+plt.xlabel(r'$t$', fontsize=fontsize)
 
 plt.tight_layout()
 plt.show()
 
 plt.subplot(2,2,1)
-plt.plot(a_est_save.mean(axis=0),label='mean')
+plt.plot(a_est_save.mean(axis=0),linewidth=2,label='mean')
 plt.fill_between(np.arange(T),np.percentile(a_est_save,97.5,axis=0),np.percentile(a_est_save,2.5,axis=0),color=u'#1f77b4',alpha=0.15,label='95% CI')
 # plt.plot(np.percentile(a_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
 # plt.plot(np.percentile(a_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
-plt.ylabel(r'$a$')
+plt.ylabel(r'$a$', fontsize=fontsize)
 plt.axhline(0.9,linestyle='--',color='k',linewidth=2.,label='true')
-plt.legend()
-plt.xlabel(r'$t$')
+
+# plt.xlabel(r'$t$')
+plt.xticks([])
 
 plt.subplot(2,2,2)
-plt.plot(b_est_save.mean(axis=0),label='mean')
+plt.plot(b_est_save.mean(axis=0),linewidth=2,label='mean')
 plt.fill_between(np.arange(T),np.percentile(b_est_save,97.5,axis=0),np.percentile(b_est_save,2.5,axis=0),color=u'#1f77b4',alpha=0.15,label='95% CI')
 # plt.plot(np.percentile(b_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
 # plt.plot(np.percentile(b_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
-plt.ylabel(r'$b$')
+plt.ylabel(r'$b$',fontsize=fontsize)
 plt.axhline(0.1,linestyle='--',color='k',linewidth=2.,label='true')
-plt.legend()
-plt.xlabel(r'$t$')
+plt.legend(fontsize=12,bbox_to_anchor=(1.15, 1.15))
+
+# plt.xlabel(r'$t$')
+plt.xticks([])
 
 plt.subplot(2,2,3)
-plt.plot(q_est_save.mean(axis=0),label='mean')
+plt.plot(q_est_save.mean(axis=0),linewidth=2,label='mean')
 plt.fill_between(np.arange(T),np.percentile(q_est_save,97.5,axis=0),np.percentile(q_est_save,2.5,axis=0),color=u'#1f77b4',alpha=0.15,label='95% CI')
 # plt.plot(np.percentile(q_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
 # plt.plot(np.percentile(q_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
-plt.ylabel(r'$q$')
+plt.ylabel(r'$q$',fontsize=fontsize)
 plt.axhline(q_true,linestyle='--',color='k',linewidth=2.,label='true')
-plt.legend()
-plt.xlabel(r'$t$')
+# plt.legend()
+plt.xlabel(r'$t$',fontsize=fontsize)
 
 plt.subplot(2,2,4)
-plt.plot(r_est_save.mean(axis=0),label='mean')
+plt.plot(r_est_save.mean(axis=0),linewidth=2,label='mean')
 plt.fill_between(np.arange(T),np.percentile(r_est_save,97.5,axis=0),np.percentile(r_est_save,2.5,axis=0),color=u'#1f77b4',alpha=0.15,label='95% CI')
 # plt.plot(np.percentile(r_est_save,97.5,axis=0), color='b',linestyle='--',linewidth=0.5,label='95% CI')
 # plt.plot(np.percentile(r_est_save,2.5,axis=0), color='b',linestyle='--',linewidth=0.5)
-plt.ylabel(r'$r$')
+plt.ylabel(r'$r$',fontsize=fontsize)
 plt.axhline(r_true,linestyle='--',color='k',linewidth=2.,label='true')
-plt.legend()
-plt.xlabel(r'$t$')
+# plt.legend()
+plt.xlabel(r'$t$',fontsize=fontsize)
 
 plt.tight_layout()
 plt.show()
+
+
+result = mpc_result_save[5]
+xt = np.reshape(x[5],(1,1))
+ut = np.array([[u[5]]])
+a = a_est_save[:,5]
+b = b_est_save[:,5]
+q = q_est_save[:,5]
+w = np.expand_dims(col_vec(q) * np.random.randn(M, N+1), 0)
+theta = {'a':a,
+         'b':b}
+uc = result['uc']
+
+if len(state_constraints) > 0:
+    x_mpc = simulate(xt, np.hstack([ut, uc]), w, theta)
+    hx = np.concatenate([state_constraint(x_mpc[:, :, 1:]) for state_constraint in state_constraints], axis=2)
+    cx = np.mean(hx > 0, axis=1)
+    print('State constraint satisfaction')
+    print(cx)
+if len(input_constraints) > 0:
+    cu = jnp.concatenate([input_constraint(uc) for input_constraint in input_constraints],axis=1)
+    print('Input constraint satisfaction')
+    print(cu >= 0)
+#
+for i in range(3):
+    plt.subplot(2,3,i+1)
+    sns.kdeplot(data=x_mpc[0, :, i*3], fill=True, alpha=.5, linewidth=0.2,label='density')
+    # plt.hist(x_mpc[0,:, i*3], label='MC forward sim',density=True)
+    plt.ylabel('')
+    if i==0:
+        plt.ylabel(r'$p(x_{t+k} | y_{1:t},u_{1:t})$', fontsize=fontsize)
+        # plt.title(r'State prediction over horizon for $t=6$')
+    plt.axvline(x_star, linestyle='--', color='g', linewidth=2, label='target')
+    plt.axvline(x_ub, linestyle='--', color='r', linewidth=2, label='constraint')
+    # if i==2:
+        # plt.legend(bbox_to_anchor=(0.85, 0.85))
+    plt.xlabel(r'$x_{t+k}$ for $k='+str(i*3+1)+'$', fontsize=fontsize)
+    plt.yticks([])
+plt.tight_layout()
+
+plt.subplot(2,1,2)
+plt.plot(np.arange(1,N+1),uc[0,:],color='k',linewidth=2.0)
+plt.axhline(u_ub, linestyle='--', color='r', linewidth=2, label='constraint')
+# plt.title(r'Control action over horizon for $t=6$')
+plt.xlabel(r'$u_{t+k}$ for $k \in [1,N]$', fontsize=fontsize)
+plt.ylabel(r'u', fontsize=fontsize)
+plt.xlim([1,10])
+plt.show()
+
+
+# axe = sns.kdeplot(data=x_mpc[0,:,:], fill=True,alpha=.5,linewidth=0.2)
+# axe.set_xlabel(r'Base arm angled (rad)')
+# axe.axvline(-0.75*np.pi,color='r',linestyle='--',linewidth=0.75)
+# plt.show()
+
