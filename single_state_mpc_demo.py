7e1f26ab8d57a77d71c37a4ea9e1be12cc5f1433
James Holdsworth <james@servitor.newcastle.edu.au>
2021-03-31 09:58:36 +1100

    pushhy

--- single_state_mpc_demo.py
+++ single_state_mpc_demo.py
@@ -24,59 +24,39 @@
 # general imports
 import pystan
 import numpy as np
-from helpers import col_vec, suppress_stdout_stderr
+from helpers import col_vec, plot_trace
 from pathlib import Path
 import pickle
-from tqdm import tqdm
+from matplotlib import pyplot as plt
 
 # jax related imports
 import jax.numpy as jnp
 from jax import grad, jit,  jacfwd, jacrev
 from jax.ops import index, index_update
 from jax.config import config
-
-# optimisation module imports (needs to be done before the jax confix update)
-from optimisation import log_barrier_cost, solve_chance_logbarrier
-
 config.update("jax_enable_x64", True)           # run jax in 64 bit mode for accuracy
 
-
-# Control parameters
-x_star = np.array([1.0])        # desired set point
-M = 200                             # number of samples we will use for MC MPC
-N = 10                              # horizonline of MPC algorithm
-sqc = np.array([[1.0]])             # square root cost on state error
-src = np.array([[0.01]])             # square root cost on control action
-delta = 0.05                        # desired maximum probability of not satisfying the constraint
-
-x_ub = 1.2
-u_ub = 2.
-state_constraints = (lambda x: x_ub - x,)
-input_constraints = (lambda u: u_ub - u,)
-
-
 # simulation parameters
-T = 30              # number of time steps to simulate and record measurements for
-x0 = 0.5            # initial time step
-r_true = 0.01       # measurement noise standard deviation
-q_true = 0.05       # process noise standard deviation
+T = 20              # number of time steps to simulate and record measurements for
+Nu = 1
+Nx = 1
+Ny = 1
+r_true = 1       # measurement noise standard deviation
+q_true = 1       # process noise standard deviation
 
 #----------------- Simulate the system-------------------------------------------#
-def ssm(x, u, a=0.9, b=0.1):
+def ssm(x, u, a=0.5, b=0.1):
     return a*x + b*u
 
 x = np.zeros(T+1)
-x[0] = x0                                   # initial state
+x[0] = 3.0                                  # initial state
 w = np.random.normal(0.0, q_true, T+1)        # make a point of predrawing noise
 y = np.zeros((T,))
 
 # create some inputs that are random but held for 10 time steps
-u = np.zeros((T+1,))     # first control action will be zero
+u = np.random.uniform(-0.5,0.5, T)
 
 ### hmc parameters and set up the hmc model
-warmup = 1000
-chains = 4
-iter = warmup + int(M/chains)
 model_name = 'single_state_gaussian_priors'
 path = 'stan/'
 if Path(path+model_name+'.pkl').is_file():
@@ -86,99 +66,31 @@ else:
     with open(path+model_name+'.pkl', 'wb') as file:
         pickle.dump(model, file)
 
-## define jax friendly function for simulating the system during mpc
-def simulate(xt, u, w, theta):
-    a = theta['a']
-    b = theta['b']
-    [o, M, N] = w.shape
-    x = jnp.zeros((o, M, N+1))
-    x = index_update(x, index[:, :,0], xt)
-    for k in range(N):
-        x = index_update(x, index[:, :, k+1], a * x[:, :, k] + b * u[:, k] + w[:, :, k])
-    return x[:, :, 1:]
+for t in range(T):
+    x[t+1] = ssm(x[t], u[t]) #+ q_true * np.random.randn()
+    y[t] = 2*x[t]
 
-# define MPC cost, gradient and hessian function
-cost = jit(log_barrier_cost, static_argnums=(11,12,13, 14, 15))  # static argnums means it will recompile if N changes
-gradient = jit(grad(log_barrier_cost, argnums=0), static_argnums=(11, 12, 13, 14, 15))    # get compiled function to return gradients with respect to z (uc, s)
-hessian = jit(jacfwd(jacrev(log_barrier_cost, argnums=0)), static_argnums=(11, 12, 13, 14, 15))
+# estimate system (estimates up to x_t)
+stan_data = {
+    'N': T,
+    'y': y,
+    'u': u
+}
+fit = model.sampling(data=stan_data, warmup=2000, iter=10000, chains=4)
+traces = fit.extract()
 
+# state samples
+z = traces['z']
 
-xt_est_save = np.zeros((1,M,T))
-a_est_save = np.zeros((M,T))
-b_est_save = np.zeros((M,T))
-q_est_save = np.zeros((M,T))
-r_est_save = np.zeros((M,T))
-mpc_result_save = []
-### SIMULATE SYSTEM AND PERFORM MPC CONTROL
-for t in tqdm(range(T),desc='Simulating system, running hmc, calculating control'):
-    # simulate system
-    # apply u_t
-    # this takes x_t to x_{t+1}
-    # measure y_t
-    x[t+1] = ssm(x[t], u[t]) + q_true * np.random.randn()
-    y[t] = x[t] + r_true * np.random.randn()
+# parameter samples
+a = traces['a']
+b = traces['b']
+c = traces['c']
+# q = traces['q']
 
-    # estimate system (estimates up to x_t)
-    stan_data = {
-        'N': t+1,
-        'y': y[:t+1],
-        'u': u[:t+1],
-        'prior_mu': np.array([0.8, 0.05, 0.1, 0.1]),
-        'prior_std': np.array([0.2, 0.2, 0.2, 0.2]),
-        'prior_state_mu': 0.3,
-        'prior_state_std': 0.2,
-    }
-    with suppress_stdout_stderr():
-        fit = model.sampling(data=stan_data, warmup=warmup, iter=iter, chains=chains)
-    traces = fit.extract()
-
-    # state samples
-    z = traces['z']
-
-    # parameter samples
-    a = traces['a']
-    b = traces['b']
-    r = traces['r']
-    q = traces['q']
-
-    # current state samples (expanded to [o,M]
-    xt = np.expand_dims(z[:, -1], 0)  # inferred state for current time step
-    # we also need to sample noise
-    w = np.expand_dims(col_vec(q) * np.random.randn(M, N + 1), 0)  # uses the sampled stds, need to sample for x_t to x_{t+N+1}
-    ut = np.expand_dims(np.array([u[t]]), 0)  # control action that was just applied
-    theta = {'a': a,
-             'b': b, }
-
-    # save some things for later plotting
-    xt_est_save[0,:,t] = z[:, -1]
-    a_est_save[:, t] = a
-    b_est_save[:, t] = b
-    q_est_save[:, t] = q
-    r_est_save[:, t] = r
-
-    # calculate next control action
-    result = solve_chance_logbarrier(np.zeros((1, N)), cost, gradient, hessian, ut, xt, theta, w, x_star, sqc, src,
-                                     delta, simulate, state_constraints, input_constraints, verbose=False)
-
-    mpc_result_save.append(result)
-    uc = result['uc']
-    u[t+1] = uc[0,0]
-
-
-run = 'single_state_demo_results'
-with open('results/'+run+'/xt_est_save.pkl','wb') as file:
-    pickle.dump(xt_est_save, file)
-with open('results/'+run+'/a_est_save.pkl','wb') as file:
-    pickle.dump(a_est_save, file)
-with open('results/'+run+'/b_est_save.pkl','wb') as file:
-    pickle.dump(b_est_save, file)
-with open('results/'+run+'/q_est_save.pkl','wb') as file:
-    pickle.dump(q_est_save, file)
-with open('results/'+run+'/r_est_save.pkl','wb') as file:
-    pickle.dump(r_est_save, file)
-with open('results/'+run+'/u.pkl','wb') as file:
-    pickle.dump(u, file)
-with open('results/'+run+'/x.pkl','wb') as file:
-    pickle.dump(x, file)
-with open('results/'+run+'/mpc_result_save.pkl', 'wb') as file:
-    pickle.dump(mpc_result_save, file)
+plot_trace(a,3,1,'a')
+plt.title('HMC inferred parameters')
+plot_trace(b,3,2,'b')
+plot_trace(c,3,3,'c')
+# plot_trace(q,4,4,'q')
+plt.show()
--- stan/single_state_gaussian_priors.stan
+++ stan/single_state_gaussian_priors.stan
@@ -2,35 +2,19 @@ data {
     int<lower=0> N;
     vector[N] y;
     vector[N] u;
-    vector[4] prior_mu;     // prior means of parameters stored a, b, q, r
-    vector[4] prior_std;    // prior stds of parameters
-    real prior_state_mu;    // first timestep state prior mean
-    real prior_state_std;   // first timestep state prior std
 }
 parameters {
     real<lower=-1.0, upper=1.0> a;       // state parameter constrained stable
     real c;
-    real<lower=0.0> r;                  // measurement noise std
-    real<lower=0.0> q;                  // process noise std
+    // real<lower=1E-8> r;                  // measurement noise std
+    // real<lower=1E-8> q;                  // process noise std
     real b;
-    vector[N] z;
+    // vector[N] z;
 }
 model {
-    // noise stds priors
-    q ~ normal(prior_mu[3], prior_std[3]);
-    r ~ normal(prior_mu[4], prior_std[4]);
-
-    // prior on parameter
-    a ~ normal(prior_mu[1], prior_std[1]);
-    b ~ normal(prior_mu[2], prior_std[2]);
-
-    // initial state prior
-    z[1] ~ normal(prior_state_mu,prior_state_std);
-
+    vector[N] z;
     // state likelihood
-    z[2:N] ~ normal(a*z[1:N-1] + b * u[1:N-1], q);
-
+    z[2:N] = a*z[1:N-1] + b*u[1:N-1];
     // measurement likelihood
-    y ~ normal(c*z, r);
-
+    y ~ normal(c*z,1);
 }
