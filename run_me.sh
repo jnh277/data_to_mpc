f996936cf0bed0583247387b16682a9803e87297
James Holdsworth <james_holdsworth@live.com.au>
2021-04-22 15:23:52 +1000

    some quick changes to the setup procedure

---
+++ run_me.sh
@@ -0,0 +1,3 @@
+pip install --upgrade pip # update pip for wheels support (mihgt be necessary)
+pip install --upgrade jax jaxlib # install CPU jax (do not do this like this for GPU support)
+pip install -r requirements.txt
