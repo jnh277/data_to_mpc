25bf56b266ac83b94cb3d6d71ed32a2bc14a6587
James Holdsworth <james.holdsworth@uon.edu.au>
2021-05-04 15:56:39 +1000

    hmc maglev example done

--- hmc_maglev.py
+++ hmc_maglev.py
@@ -13,7 +13,7 @@ if platform.system()=='Darwin':
 import pystan
 import numpy as np
 import matplotlib.pyplot as plt
-from helpers import plot_trace
+from helpers import plot_trace_grid
 from pathlib import Path
 import pickle
 
@@ -25,176 +25,199 @@ from jax.ops import index, index_update
 from jax.config import config
 config.update("jax_enable_x64", True)
 
-plot_bool = True
-#----------------- Parameters ---------------------------------------------------#
+if __name__ == "__main__":
+    plot_bool = True
+    #----------------- Parameters ---------------------------------------------------#
 
-T = 50             # number of time steps to simulate and record measurements for
-Ts = 0.004
-# true (simulation) parameters
-z1_0 = 0.009  # initial position
-z2_0 = 0.0  # initial velocity
-r1_true = 0.00002 # measurement noise standard deviation
-q1_true = 0.0005 * Ts # process noise standard deviation
-q2_true = 0.00005 * Ts 
+    T = 500             # number of time steps to simulate and record measurements for
+    Ts = 0.0004
+    # true (simulation) parameters
+    z1_0 = 0.009  # initial position
+    z2_0 = 0.0  # initial velocity
+    r1_true = 0.0000002 # measurement noise standard deviation
+    q1_true = 0.0005 * Ts # process noise standard deviation
+    q2_true = 0.00005 * Ts 
 
-# got these values from the data sheet
-Mb_true = 0.06 # kg, mass of the steel ball
-Ldiff_true = 0.04 # H, difference between coil L at zero and infinity (L(0) - L(inf))
-x50_true = 0.002 # m, location in mode of L where L is 50% of the infinity and 0 L's
-grav = 9.81
-k0_true = 0.5*x50_true*Ldiff_true/Mb_true
-I0_true = x50_true
-theta_true = {
-        'Mb': Mb_true,
-        'Ldiff': Ldiff_true,
-        'x50': x50_true,
-        'I0': I0_true,
-        'k0': k0_true,
-        'g': grav,
-        'h': Ts
-}
-Nx = 2
-Ny = 1
-Nu = 1
-
-#----------------- Simulate the system-------------------------------------------#
-
-def maglev_gradient(xt, u, t): # uses the lumped parametersation 
-    dx = jnp.zeros_like(xt)
-    dx = index_update(dx, index[0, :], xt[1, :])
-    dx = index_update(dx, index[1, :], t['g'] - t['k0'] * u * u / (t['I0'] + xt[0,:]) / (t['I0'] + xt[0,:]))
-    return dx
-
-def current_current(xt,t): # expect a slice of x_hist to be xt, shape (2,)
-    return np.sqrt(t['g']/t['k0'])*(xt[0] + t['I0'])
-
-def rk4(xt, ut, theta):
-    h = theta['h']
-    k1 = maglev_gradient(xt, ut, theta)
-    k2 = maglev_gradient(xt + k1 * h / 2, ut, theta)
-    k3 = maglev_gradient(xt + k2 * h / 2, ut, theta)
-    k4 = maglev_gradient(xt + k3 * h, ut, theta)
-    return xt + (k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6) * h  # should handle a 2D x just fine
-
-def simulate(xt, u, w,
-                  theta):  # w is expected to be 3D. xt is expected to be 2D. ut is expected to be 2d but also, should handle being a vector (3d)
-    [Nx, Ns, Np1] = w.shape
-    x = jnp.zeros((Nx, Ns, Np1 + 1))
-    x = index_update(x, index[:, :, 0], xt)
-    iis = jnp.arange(Np1)
-    dict = {
-        'x':x,
-        'u':u,
-        'w':w,
-        'theta':theta
+    # got these values from the data sheet
+    Mb_true = 0.06 # kg, mass of the steel ball
+    Ldiff_true = 0.04 # H, difference between coil L at zero and infinity (L(0) - L(inf))
+    x50_true = 0.002 # m, location in mode of L where L is 50% of the infinity and 0 L's
+    grav = 9.81
+    k0_true = 0.5*x50_true*Ldiff_true/Mb_true
+    I0_true = x50_true
+    theta_true = {
+            'Mb': Mb_true,
+            'Ldiff': Ldiff_true,
+            'x50': x50_true,
+            'I0': I0_true,
+            'k0': k0_true,
+            'g': grav,
+            'h': Ts
     }
-    dict,_ = scan(scan_func,dict,iis) # carry, ys = scan(scan_func,dict,iis). for each ii in iis, runs dict,y = scan_func(dict,ii) and ys = ys.append(y)
-    return dict['x'][:, :, 1:]  # return everything except xt
+    Nx = 2
+    Ny = 1
+    Nu = 1
 
-def scan_func(carry,ii):
-    carry['x'] = index_update(carry['x'], index[:, :, ii + 1], rk4(carry['x'][:, :, ii], carry['u'][:, ii], carry['theta']) + carry['w'][:, :, ii])
-    return carry, []
+    #----------------- Simulate the system-------------------------------------------#
+
+    def maglev_gradient(xt, u, t): # uses the lumped parametersation 
+        dx = jnp.zeros_like(xt)
+        dx = index_update(dx, index[0, :], xt[1, :])
+        dx = index_update(dx, index[1, :], t['g'] - t['k0'] * u * u / (t['I0'] + xt[0,:]) / (t['I0'] + xt[0,:]))
+        return dx
+
+    def current_current(xt,t): # expect a slice of x_hist to be xt, shape (2,)
+        return np.sqrt(t['g']/t['k0'])*(xt[0] + t['I0'])
+
+    def rk4(xt, ut, theta):
+        h = theta['h']
+        k1 = maglev_gradient(xt, ut, theta)
+        k2 = maglev_gradient(xt + k1 * h / 2, ut, theta)
+        k3 = maglev_gradient(xt + k2 * h / 2, ut, theta)
+        k4 = maglev_gradient(xt + k3 * h, ut, theta)
+        return xt + (k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6) * h  # should handle a 2D x just fine
+
+    @jit
+    def simulate(xt, u, w,
+                    theta):  # w is expected to be 3D. xt is expected to be 2D. ut is expected to be 2d but also, should handle being a vector (3d)
+        [Nx, Ns, Np1] = w.shape
+        x = jnp.zeros((Nx, Ns, Np1 + 1))
+        x = index_update(x, index[:, :, 0], xt)
+        iis = jnp.arange(Np1)
+        dict = {
+            'x':x,
+            'u':u,
+            'w':w,
+            'theta':theta
+        }
+        dict,_ = scan(scan_func,dict,iis) # carry, ys = scan(scan_func,dict,iis). for each ii in iis, runs dict,y = scan_func(dict,ii) and ys = ys.append(y)
+        return dict['x'][:, :, 1:]  # return everything except xt
+
+    def scan_func(carry,ii):
+        carry['x'] = index_update(carry['x'], index[:, :, ii + 1], rk4(carry['x'][:, :, ii], carry['u'][:, ii], carry['theta']) + carry['w'][:, :, ii])
+        return carry, []
 
 
-z_sim = np.zeros((Nx,1,T+1), dtype=float) # state history allocation
+    z_sim = np.zeros((Nx,1,T+1), dtype=float) # state history allocation
 
-# load initial state
-z_sim[0,0,0] = z1_0 
-z_sim[1,0,0] = z2_0 
+    # load initial state
+    z_sim[0,0,0] = z1_0 
+    z_sim[1,0,0] = z2_0 
 
-# noise is predrawn and independant
-w_sim = np.zeros((Nx,1,T),dtype=float)
-w_sim[0,0,:] = np.random.normal(0.0, q1_true, T)
-w_sim[1,0,:] = np.random.normal(0.0, q2_true, T)
+    # noise is predrawn and independant
+    w_sim = np.zeros((Nx,1,T),dtype=float)
+    w_sim[0,0,:] = np.random.normal(0.0, q1_true, T)
+    w_sim[1,0,:] = np.random.normal(0.0, q2_true, T)
 
-# create some inputs that are a step from one equilibrium to another
-u = current_current(z_sim[:,0,0],theta_true) * np.ones((Nu,T), dtype=float)
-u[:,25:] = u[:,25:] - 0.001
+    # create some inputs that are a step from one equilibrium to another
+    # u_noise = 0.001*np.sin(np.arange(500)*2*np.pi*5/500)
+    # u = current_current(z_sim[:,0,0],theta_true) * np.ones((Nu,T), dtype=float)
+    # u += u_noise
+    # some random error state PID I made up
+    Kp = 100
+    Ki = 5
+    Kd = 3
+    reference = z1_0 - 0.002
+    u = np.zeros((Nu,T), dtype=float)
+    error = z1_0 - reference
+    integrator = 0
+    difference = 0
+    u[:,0] = current_current(z_sim[:,0,0],theta_true) + Kp * error +  Ki * integrator + Kd * difference
+    for k in np.arange(T):
+        z_sim[:,:,[k+1]] = simulate(z_sim[:,:,k],u[:,[k]],w_sim[:,:,[k]],theta_true)
+        if k < T - 1:
+            difference = -error
+            error = z_sim[0,:,k + 1] - reference
+            integrator += error * Ts
+            difference += error
+            difference /= Ts
+            u[:,k+1] = current_current(z_sim[:,0,k+1],theta_true) + Kp * error +  Ki * integrator + Kd * difference
 
-z_sim[:,:,1:] = simulate(z_sim[:,:,0],u,w_sim,theta_true)
+    # draw measurement noise
+    v = np.zeros((Ny,T), dtype=float)
+    v[0,:] = np.random.normal(0.0, r1_true, T)
 
-# draw measurement noise
-v = np.zeros((Ny,T), dtype=float)
-v[0,:] = np.random.normal(0.0, r1_true, T)
+    # simulated measurements 
+    y = np.zeros((Ny,T), dtype=float)
+    y[0,:] = z_sim[0,0,:-1]
+    y = y + v; # add noise to measurements
 
-# simulated measurements 
-y = np.zeros((Ny,T), dtype=float)
-y[0,:] = z_sim[0,0,:-1]
-y = y + v; # add noise to measurements
+    plt.subplot(2,1,1)
+    plt.plot(u[0,:])
+    plt.title('Simulated inputs and measurement used for inference')
+    plt.subplot(2, 1, 2)
+    plt.plot(z_sim[0,:])
+    plt.plot(y[0,:],linestyle='None',color='r',marker='*')
+    plt.title('Simulated state 1 and measurements used for inferences')
+    plt.tight_layout()
+    plt.show()
 
-plt.subplot(2,1,1)
-plt.plot(u[0,:])
-plt.title('Simulated inputs and measurement used for inference')
-plt.subplot(2, 1, 2)
-plt.plot(z_sim[0,:])
-plt.plot(y[0,:],linestyle='None',color='r',marker='*')
-plt.title('Simulated state 1 and measurements used for inferences')
-plt.tight_layout()
-plt.show()
+    #----------- USE HMC TO PERFORM INFERENCE ---------------------------#
+    # avoid recompiling
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    model_name = 'maglev'
+    path = '/stan/'
+    if Path(script_path+path+model_name+'.pkl').is_file():
+        model = pickle.load(open(script_path+path+model_name+'.pkl', 'rb'))
+    else:
+        model = pystan.StanModel(file=script_path+path+model_name+'.stan')
+        with open(script_path+path+model_name+'.pkl', 'wb') as file:
+            pickle.dump(model, file)
 
-# #----------- USE HMC TO PERFORM INFERENCE ---------------------------#
-# # avoid recompiling
-# script_path = os.path.dirname(os.path.realpath(__file__))
-# model_name = 'maglev'
-# path = '/stan/'
-# if Path(script_path+path+model_name+'.pkl').is_file():
-#     model = pickle.load(open(script_path+path+model_name+'.pkl', 'rb'))
-# else:
-#     model = pystan.StanModel(file=script_path+path+model_name+'.stan')
-#     with open(script_path+path+model_name+'.pkl', 'wb') as file:
-#         pickle.dump(model, file)
+    stan_data = {
+        'no_obs': T,
+        'N':T,
+        'y':y[0,:],
+        'u':u[0,:],
+        'g':grav,
+        'z0_mu': np.array([z1_0,z2_0]),
+        'z0_std': np.array([q1_true,q2_true]),
+        'theta_p_mu': np.array([I0_true, k0_true]),
+        'theta_p_std':0.05*np.array([I0_true, k0_true]),
+        'r_p_mu': np.array([r1_true]),
+        'r_p_std': 0.05*np.array([r1_true]),
+        'q_p_mu': np.array([q1_true, q2_true]),
+        'q_p_std': 0.05*np.array([q1_true, q2_true]),
+        'Ts':Ts
+    }
 
-# stan_data = {
-#     'N':T,
-#     'y':y,
-#     'u':u,
-#     'g':grav,
-    # 'theta_p_mu':theta_p_mu,
-    # 'theta_p_std':0.0005*np.array([I0_true, k0_true]),
-    # 'r_p_mu': np.array([r1_true]),
-    # 'r_p_std': 0.0005*np.array([r1_true]),
-    # 'q_p_mu': np.array([q1_true, q2_true]),
-    # 'q_p_std': 0.0005*np.array([q1_true, q2_true]),
-#     'Ts':Ts
-# }
+    fit = model.sampling(data=stan_data, warmup=1000, iter=2000, chains=1)
+    traces = fit.extract()
 
-# fit = model.sampling(data=stan_data, warmup=1000, iter=2000)
-# traces = fit.extract()
+    # state samples
+    z_samps = np.transpose(traces['h'],(1,0,2)) # Ns, Nx, T --> Nx, Ns, T
+    theta_samps = traces['theta']
 
-# # state samples
-# z_samps = np.transpose(traces['z'],(1,0,2)) # Ns, Nx, T --> Nx, Ns, T
-# theta_samps = traces['theta']
+    # parameter samples
+    I0_samps = theta_samps[:, 0].squeeze()
+    k0_samps = theta_samps[:, 1].squeeze()
+    r_samps = traces['r'].transpose()
+    q_samps = traces['q'].transpose()
 
-# # parameter samples
-# I0_samps = theta_samps[:, 0].squeeze()
-# k0_samps = theta_samps[:, 1].squeeze()
-# r_samps = traces['r']
-# q_samps = traces['q']
+    # plot the initial parameter marginal estimates
+    q1plt = q_samps[0,:].squeeze()
+    q2plt = q_samps[1,:].squeeze()
+    r1plt = r_samps[0,:].squeeze()
 
-# # plot the initial parameter marginal estimates
-# q1plt = q_samps[0,:].squeeze()
-# q2plt = q_samps[1,:].squeeze()
-# r1plt = r_samps[0,:].squeeze()
+    plot_trace_grid(I0_samps,1,5,1,'I0')
+    plt.title('HMC inferred parameters')
+    plot_trace_grid(k0_samps,1,5,2,'k0')
+    plot_trace_grid(q1plt,1,5,3,'q1')
+    plot_trace_grid(q1plt,1,5,4,'q2')
+    plot_trace_grid(r1plt,1,5,5,'r1')
+    plt.show()
 
-# plot_trace_grid(I0_samps,1,5,1,'I0')
-# plt.title('HMC inferred parameters')
-# plot_trace_grid(k0_samps,1,5,2,'k0')
-# plot_trace_grid(q1plt,1,5,3,'q1')
-# plot_trace_grid(q1plt,1,5,4,'q2')
-# plot_trace_grid(r1plt,1,5,5,'r1')
-# plt.show()
-
-# # plot some of the initial marginal state estimates
-# for i in range(4):
-#     if i==1:
-#         plt.title('HMC inferred position')
-#     plt.subplot(2,2,i+1)
-#     plt.hist(z_samps[0,:,i*20+1],bins=30, label='p(x_'+str(i+1)+'|y_{1:T})', density=True)
-#     plt.axvline(z_sim[0,i*20+1], label='True', linestyle='--',color='k',linewidth=2)
-#     plt.xlabel('x_'+str(i+1))
-# plt.tight_layout()
-# plt.legend()
-# plt.show()
+    # # plot some of the initial marginal state estimates
+    # for i in range(4):
+    #     if i==1:
+    #         plt.title('HMC inferred position')
+    #     plt.subplot(2,2,i+1)
+    #     plt.hist(z_samps[0,:,i*20+1],bins=30, label='p(x_'+str(i+1)+'|y_{1:T})', density=True)
+    #     plt.axvline(z_sim[0,i*20+1], label='True', linestyle='--',color='k',linewidth=2)
+    #     plt.xlabel('x_'+str(i+1))
+    # plt.tight_layout()
+    # plt.legend()
+    # plt.show()
 
 
 
---
+++ stan/maglev.stan
@@ -0,0 +1,79 @@
+// stan model for the maglev
+
+functions{
+    matrix process_model_vec(matrix z, row_vector u, vector theta, real g){
+    // theta = [k0, I0]
+    // x = [ball position from magnet, ball velocity]
+        int pdims[2] = dims(z);
+        matrix[pdims[1],pdims[2]] dz;
+        real k0 = theta[2];
+        real I0 = theta[1];
+        // row_vector[pdims[2]] dz2 = 
+        dz[1,:] = z[2,:];
+        dz[2,:] = g - k0 * u .* u ./((z[1,:] + I0).*(z[1,:] + I0));
+        return dz;
+    }
+
+    matrix rk4_update(matrix z, row_vector u, vector theta, real g, real Ts){
+        int pdims[2] = dims(z);
+        matrix[pdims[1],pdims[2]] k1;
+        matrix[pdims[1],pdims[2]] k2;
+        matrix[pdims[1],pdims[2]] k3;
+        matrix[pdims[1],pdims[2]] k4;
+        k1 = process_model_vec(z, u, theta, g);
+        k2 = process_model_vec(z+Ts*k1/2, u, theta, g);
+        k3 = process_model_vec(z+Ts*k2/2, u, theta, g);
+        k4 = process_model_vec(z+Ts*k3, u, theta, g);
+        return z + Ts*(k1/6 + k2/3 + k3/3 + k4/6);
+    }
+
+}
+
+data {
+    int<lower=0> no_obs;
+    row_vector[no_obs] y;                // measurement [x, y, theta]
+    row_vector[no_obs] u;               // input
+    real<lower=0> Ts;                   // time step
+    real g;                             // gravity
+    vector[2] theta_p_mu;               // prior means on theta
+    vector[2] theta_p_std;              // prior stds on theta
+    vector[2] z0_mu;                    // initial state prior
+    vector[2] z0_std;
+    vector[1] r_p_mu;                   // measurement noise prior mean
+    vector[1] r_p_std;                  // measurement noise prior std
+    vector[2] q_p_mu;                   // process noise prior mean
+    vector[2] q_p_std;                  // process noise prior std
+}
+parameters {
+    matrix[2,no_obs+1] h;                     // hidden states
+    vector<lower=0.0>[2] theta;             // the parameters  [I0, k0]
+    vector<lower=1e-8>[1] r;                 // independent measurement noise variances
+    vector<lower=1e-8>[2] q;                 // independent process noise variances
+}
+transformed parameters {
+    matrix[2, no_obs] mu;
+    matrix[1, no_obs] yhat;
+    // process model
+    mu = rk4_update(h[:,1:no_obs], u[1:no_obs], theta, g, Ts); // this option was used for results in paper
+    // measurement model
+    yhat[1,:] = h[1,1:no_obs];
+}
+model {
+    r ~ normal(r_p_mu, r_p_std);
+    q ~ normal(q_p_mu, q_p_std);
+
+    // parameter priors
+    theta ~ normal(theta_p_mu, theta_p_std);
+
+    // initial state prior (Not using this / not needed)
+    h[:,1] ~ normal(z0_mu, z0_std);
+
+    // independent process likelihoods
+    h[1,2:no_obs+1] ~ normal(mu[1,:], q[1]);
+    h[2,2:no_obs+1] ~ normal(mu[2,:], q[2]);
+
+    // independent measurement likelihoods
+    y[:] ~ normal(yhat[1,:], r[1]);
+}
+
+
